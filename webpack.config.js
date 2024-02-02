4b0bc090f857932d31bf5715f83a2495a42597e2
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2024-02-02 10:57:05 +0800

    set webpack config mode to none

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
+  mode: 'none',
   entry: '/src/index.js',
   output: {
     filename: "main.js",
