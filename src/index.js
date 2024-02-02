88f9f53e412771c4f405c0d7e91b839a93ee2492
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2024-02-02 09:42:00 +0800

    entry point Updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 import 'lodash';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-
-
-// function component() {
-//     const element = document.createElement('div');
-  
-//   // Lodash, now imported by this script
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-//     return element;
-//   }
-  
-//   document.body.appendChild(component());
-
-// console.log('Hello World!');
